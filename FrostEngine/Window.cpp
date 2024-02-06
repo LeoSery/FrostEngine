f3d76e7544eba22a50e46a0ce98ad60d3b156e0a
Séry Léo <74504632+LeoSery@users.noreply.github.com>
2024-02-06 12:04:19 +0100

    Add: Empty window class

---
+++ FrostEngine/Window.cpp
@@ -0,0 +1,8 @@
+#include "Window.h"
+
+namespace frost
+{
+	Window::Window(std::string_view /*name*/, const glm::vec2& /*size*/)
+	{
+	}
+}
---
+++ FrostEngine/Window.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Export.h"
+
+#include <glm/vec2.hpp>
+
+#include <string_view>
+
+namespace frost
+{
+	class FROST_ENGINE_API Window
+	{
+		explicit Window(std::string_view name, const glm::vec2& size);
+		~Window() = default;
+	};
+}
