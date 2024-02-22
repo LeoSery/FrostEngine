f13349ce57ec409cfb61486140c2800339478995
HideNknow <outin.matthieu@gmail.com>
2024-02-22 13:45:37 +0100

    Add : Shaders class
    Shader Class load shaders and can bind them when needed.

--- FrostEngine/src/Render/Shader.cpp
+++ FrostEngine/src/Render/Shader.cpp
@@ -1,1 +1,73 @@
 #include "Render/Shader.h"
+#include <filesystem>
+
+namespace frost::core
+{
+	Shader::Shader()
+	{
+	}
+
+	void Shader::InitShader(const char* _vertexFile, const char* _fragmentFile)
+	{
+		//this return a string but we need a const char*
+		std::string vertShaderSrc = ReadShaderFile(_vertexFile);
+		std::string fragShaderSrc = ReadShaderFile(_fragmentFile);
+
+		const char* vertShader = vertShaderSrc.c_str();
+		const char* fragShader = fragShaderSrc.c_str();
+
+		//Create Vertex shader from src and compile it
+		GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vs, 1, &vertShader, nullptr);
+		glCompileShader(vs);
+
+		//Create Fragment shader from src and compile it
+		GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fs, 1, &fragShader, nullptr);
+		glCompileShader(fs);
+
+		//Create program, attach it shaders and link it
+		ID = glCreateProgram();
+		glAttachShader(ID, vs);
+		glAttachShader(ID, fs);
+		glLinkProgram(ID);
+
+		glDeleteShader(vs);
+		glDeleteShader(fs);
+
+	}
+
+	void Shader::Enable()
+	{
+		glUseProgram(ID);
+	}
+
+	void Shader::Delete()
+	{
+		glDeleteProgram(ID);
+	}
+	
+	std::string Shader::ReadShaderFile(const char* _filename)
+	{
+		// Get the current working directory 
+		std::filesystem::path cwd = std::filesystem::current_path();
+
+		// Construct a file path relative to the current working directory 
+		std::filesystem::path file_path = cwd / "ressources" / "shaders" / _filename;
+
+		std::cout << file_path << std::endl;
+		std::ifstream in(file_path, std::ios::binary);
+
+		if (in)
+		{
+			std::string contents;
+			in.seekg(0, std::ios::end);
+			contents.resize(in.tellg());
+			in.seekg(0, std::ios::beg);
+			in.read(&contents[0], contents.size());
+			in.close();
+			return(contents);
+		}
+		throw(errno);
+	}
+}
---
+++ Game/ressources/shaders/default.vert
@@ -0,0 +1,18 @@
+#version 460 core
+layout(location = 0) in vec2 aPos;
+layout(location = 1) in vec3 aColor;
+out vec3 vColor;
+
+uniform vec2  uPosition;
+uniform float uRotation;
+uniform vec2  uScale;
+uniform float uAspectRatio;
+
+void main()
+{
+	mat2 rotMatrix = mat2(cos(uRotation), -sin(uRotation), sin(uRotation), cos(uRotation));
+	vec2 finalPos = ((rotMatrix * aPos) * uScale + uPosition) / vec2(uAspectRatio, 1.0);
+
+	gl_Position = vec4(finalPos, 0.0, 1.0);
+	vColor = aColor;
+}
