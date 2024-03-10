2ef89b8f5ce66a9acc4c0f3672e5ab49cd36027c
Séry Léo <74504632+LeoSery@users.noreply.github.com>
2024-03-10 09:19:05 +0100

    Update: The class used to make the engine demonstrator game to use the new engine architecture

---
+++ Game/src/Game.cpp
@@ -0,0 +1,51 @@
+#include "FrostEngine.h"
+
+#include <windows.h>
+
+#pragma region "Alias"
+using SceneManager = frost::core::SceneManager;
+using GameObject = frost::ECS::GameObject;
+using Transform = frost::ECS::Transform;
+using Logger = frost::utils::Logger;
+#pragma endregion
+
+class MyGameApp : public FrostEngine::Application
+{
+public:
+	void Init() override
+	{
+		std::string MainSceneName = "GameScene";
+		SceneManager::GetInstance().CreateScene(MainSceneName);
+		SceneManager::GetInstance().LoadScene(MainSceneName);
+
+		Application::Init({ "Mon super jeu", glm::ivec2(1600, 900) });
+
+		Logger::LogInfo("Creating 2 GameObjects and set their pos");
+		GameObject* GameObjectTest = new GameObject("GameObjectTest");
+		Transform* TransformC = GameObjectTest->GetComponent<Transform>();
+		TransformC->position = glm::vec2(1, 2);
+
+		GameObjectTest->AddTag("Tag1");
+		GameObjectTest->AddTag("Tag2");
+
+		GameObject* GameObjectTest2 = new GameObject("GameObjectTest2");
+		Transform* TransformC2 = GameObjectTest2->GetComponent<Transform>();
+		TransformC2->position = glm::vec2(1, 2);
+
+		GameObjectTest->GetData(true);
+		GameObjectTest2->GetData(true);
+	}
+
+	void Update(float deltaTime) override
+	{
+		(void)deltaTime; //this is to avoid warning when delta time is not used
+		// To do : update game logic (one frame)
+	}
+
+	void Shutdown() override
+	{
+		Application::Shutdown();
+	}
+};
+
+FROST_APPLICATION(MyGameApp)
