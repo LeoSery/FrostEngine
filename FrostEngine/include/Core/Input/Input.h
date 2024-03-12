185b8334fbf31d0e29a4723c85eaf9d123c289ec
HideNknow <outin.matthieu@gmail.com>
2024-03-12 17:34:09 +0100

    Add: input

--- FrostEngine/include/Core/Input/Input.h
+++ FrostEngine/include/Core/Input/Input.h
@@ -1,19 +1,31 @@
 #pragma once
 
 #include "Core/Export.h"
+#include "IInputable.h"
+
+
 #include <GLFW/glfw3.h>
+
 #include <functional>
 #include <string>
 #include <memory>
 #include <vector>
 #include <map>
 
+
+
+
 namespace frost::core 
 {
 	class Window;
 
-	class FROST_ENGINE_API Input
+	class FROST_ENGINE_API Input : public IInputable
 	{
+		
+
+	protected: 
+		static Input* m_Instance;
+
 	public:
 		// Constructor and Destructors
         Input(Input& other) = delete;
@@ -25,13 +37,6 @@ namespace frost::core
 
 		// Methods
 		Input* init(Window* _window);
-		void AddAction(std::string _actionName);
-		bool AddActionToKey(int key, std::string _actionName);//return true if succesfully added
-		void AddFunctionToAction(std::string _actionName, std::function<void()> _functionToCall);
-		void CallAction(std::string _actionToCall);
-
-	protected:
-		static Input* m_instance;
 
 		//forward Declaration for pimple idiom BUT IT'S NOT WORKING
 	private:
@@ -42,9 +47,17 @@ namespace frost::core
 		};
 
 		std::unique_ptr<Internal> m_internal;
+
 		std::map<int, std::string> KeyBind; //key = Action
 		std::map<std::string,std::vector<std::function<void()>> > ActionBind; //Action = Function to call
 
+		void AddAction(std::string ActionName);
+
+		bool AddKeyToAction(int key, std::string ActionName);//return true if succesfully added
+
+	public:
+
 		static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
 	};
 }
--- FrostEngine/src/Core/Input/Input.cpp
+++ FrostEngine/src/Core/Input/Input.cpp
@@ -8,24 +8,25 @@
 
 namespace frost::core
 {
-	Input* Input::m_instance = nullptr;
+	Input* Input::m_Instance = nullptr;
 
 	Input::Input() : m_internal(new Internal)
 	{}
 
 	Input* Input::GetInstance()
 	{
-		if (m_instance == nullptr) {
-			m_instance = new Input;
+		if (m_Instance == nullptr) {
+			m_Instance = new Input;
 		}
-		return m_instance;
+		return m_Instance;
 	}
 
 	Input* Input::init(Window* _window)
 	{
+
 		m_internal->window = static_cast<GLFWwindow*>(_window->GetInternal());
 		glfwSetKeyCallback(m_internal->window,key_callback);
-		return m_instance;
+		return m_Instance;
 	}
 
 	void Input::key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
@@ -36,66 +37,6 @@ namespace frost::core
 		mods;
 		window;
 
-		if (m_instance->KeyBind.contains(key))
-		{
-			m_instance->CallAction(m_instance->KeyBind.at(key));
-		}
+		
 	}
-
-	void Input::AddAction(std::string _actionName)
-	{
-		if (! m_instance->ActionBind.contains(_actionName))
-		{
-			std::pair<std::string, std::vector<std::function<void()>>> toadd(_actionName, std::vector<std::function<void()>>());
-			m_instance->ActionBind.insert(toadd);
-		}
-		else
-		{
-			frost::utils::Logger::LogWarning("Can't add " + _actionName + " already exists");
-		}
-	}
-
-	bool Input::AddActionToKey(int key, std::string _actionName)
-	{
-
-		if (m_instance->ActionBind.contains(_actionName))
-		{
-			if (m_instance->KeyBind.contains(key))
-			{
-				m_instance->KeyBind.at(key) = _actionName;
-			}
-			else
-			{
-				std::pair<int, std::string> toadd(key, _actionName);
-				m_instance->KeyBind.insert(toadd);
-			}
-			return true;
-		}
-		else
-		{
-			frost::utils::Logger::LogWarning("Action " + _actionName + " does not exist");
-		}
-		return false;
-	}
-
-	void Input::AddFunctionToAction(std::string _actionName, std::function<void()> _functionToCall)
-	{
-		if (m_instance->ActionBind.contains(_actionName))
-		{
-			m_instance->ActionBind.at(_actionName).push_back(_functionToCall);
-		}
-		else
-		{
-			frost::utils::Logger::LogError("Action " + _actionName + " does not exist");
-		}
-	}
-
-	void Input::CallAction(std::string _actionToCall)
-	{
-		for (size_t i = 0; i < m_instance->ActionBind.at(_actionToCall).size(); i++)
-		{
-			m_instance->ActionBind.at(_actionToCall).at(i)();
-		}
-	}
-
 }
