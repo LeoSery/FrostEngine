a2a0b43ef7551268b836a4856b6f4b8424d35fcd
Séry Léo <74504632+LeoSery@users.noreply.github.com>
2024-03-22 00:01:18 +0100

    Update: Collision system. Add: Gameplay basics

--- FrostEngine/src/ECS/GameObject.cpp
+++ FrostEngine/src/ECS/GameObject.cpp
@@ -8,158 +8,163 @@
 
 namespace frost::ECS
 {
-    GameObject::GameObject(std::string _Name, GameObject* _Parent)
-        : Tree<GameObject>(_Parent/* ? _Parent : core::SceneManager::GetInstance().GetActiveScene().GetRoot()*/)
-        , m_name(std::move(_Name))
-        , m_uuid(uuids::uuid_system_generator{}())
-    {
-    }
+	GameObject::GameObject(std::string _Name, GameObject* _Parent)
+		: Tree<GameObject>(_Parent/* ? _Parent : core::SceneManager::GetInstance().GetActiveScene().GetRoot()*/)
+		, m_name(std::move(_Name))
+		, m_uuid(uuids::uuid_system_generator{}())
+	{
+	}
 
-    void GameObject::Init()
-    {
-        AddComponent<Transform>();
-        SetActive(true);
-        Start();
-    }
+	void GameObject::Init()
+	{
+		AddComponent<Transform>();
+		SetActive(true);
+		Start();
+	}
 
-    GameObject* GameObject::New(std::string _Name, GameObject* _Parent)
-    {
-        GameObject* result = new GameObject(_Name, _Parent);
-        result->Init();
-        return result;
-    }
+	GameObject* GameObject::New(std::string _Name, GameObject* _Parent)
+	{
+		GameObject* result = new GameObject(_Name, _Parent);
+		result->Init();
+		return result;
+	}
 
-    GameObject::~GameObject()
-    {
-        //Destroy();
-        for (const IComponent* component : m_components)
-        {
-            delete component;
-        }
+	GameObject::~GameObject()
+	{
+		//Destroy();
+		for (const IComponent* component : m_components)
+		{
+			delete component;
+		}
 
-        m_components.clear();
-    }
+		m_components.clear();
+	}
 
-    void GameObject::Start()
-    {
-        for (IComponent* component : m_components)
-        {
-            component->Start();
-        }
-    }
+	void GameObject::Start()
+	{
+		for (IComponent* component : m_components)
+		{
+			component->Start();
+		}
+	}
 
-    void GameObject::Update(float _DeltaTime)
-    {
-        for (IComponent* component : m_components)
-        {
-            component->Update(_DeltaTime);
-        }
-    }
+	void GameObject::Update(float _DeltaTime)
+	{
+		for (IComponent* component : m_components)
+		{
+			component->Update(_DeltaTime);
+		}
+	}
 
-    void GameObject::Destroy()
-    {
-        for (IComponent* component : m_components)
-        {
-            component->Destroy();
-        }
-    }
+	void GameObject::Destroy()
+	{
+		for (IComponent* component : m_components)
+		{
+			component->Destroy();
+		}
+	}
 
-    const std::string& GameObject::GetName() const
-    {
-        return m_name;
-    }
+	const std::string& GameObject::GetName() const
+	{
+		return m_name;
+	}
 
-    void GameObject::SetName(const std::string& _Name)
-    {
-        m_name = _Name;
-    }
+	void GameObject::SetName(const std::string& _Name)
+	{
+		m_name = _Name;
+	}
 
-    const Transform& GameObject::GetTransform() const
-    {
-        return *GetComponent<Transform>();
-    }
+	Transform& GameObject::GetTransform()
+	{
+		return *GetComponent<Transform>();
+	}
 
-    void GameObject::SetTransform(const Transform& _Transform)
-    {
-        Transform& transformComponent = *GetComponent<Transform>();
+	const Transform& GameObject::GetTransform() const
+	{
+		return *GetComponent<Transform>();
+	}
 
-        transformComponent.position = _Transform.position;
-        transformComponent.rotation = _Transform.rotation;
-        transformComponent.scale = _Transform.scale;
-    }
+	void GameObject::SetTransform(const Transform& _Transform)
+	{
+		Transform& transformComponent = *GetComponent<Transform>();
 
-    void GameObject::AddTag(const std::string& _Tag)
-    {
-        m_tags.insert(_Tag);
-    }
+		transformComponent.position = _Transform.position;
+		transformComponent.rotation = _Transform.rotation;
+		transformComponent.scale = _Transform.scale;
+	}
 
-    bool GameObject::HasTag(const std::string& _Tag) const
-    {
-        return m_tags.find(_Tag) != m_tags.end();
-    }
+	void GameObject::AddTag(const std::string& _Tag)
+	{
+		m_tags.insert(_Tag);
+	}
 
-    void GameObject::RemoveTag(const std::string& _Tag)
-    {
-        m_tags.erase(_Tag);
-    }
+	bool GameObject::HasTag(const std::string& _Tag) const
+	{
+		return m_tags.find(_Tag) != m_tags.end();
+	}
 
-    bool GameObject::IsActive() const
-    {
-        return m_isActive;
-    }
+	void GameObject::RemoveTag(const std::string& _Tag)
+	{
+		m_tags.erase(_Tag);
+	}
 
-    void GameObject::SetActive(bool _IsActive)
-    {
-        m_isActive = _IsActive;
-    }
+	bool GameObject::IsActive() const
+	{
+		return m_isActive;
+	}
 
-    uuids::uuid GameObject::GetUUID() const
-    {
-        return m_uuid;
-    }
+	void GameObject::SetActive(bool _IsActive)
+	{
+		m_isActive = _IsActive;
+	}
 
-    void GameObject::GetData(bool _ForceLoggerDraw) const
-    {
-        frost::utils::Logger* Logger = frost::utils::Logger::GetInstance();
+	uuids::uuid GameObject::GetUUID() const
+	{
+		return m_uuid;
+	}
 
-        Logger->LogInfo(m_name + " Infos: ");
-        Logger->LogInfo("- Parent: " + (GetParent() ? GetParent()->GetName() : "None"));
-        Logger->LogInfo(std::format("- UUID: {}", uuids::to_string(m_uuid)));
+	void GameObject::GetData(bool _ForceLoggerDraw) const
+	{
+		frost::utils::Logger* Logger = frost::utils::Logger::GetInstance();
 
-        Logger->LogInfo("- Name: " + m_name);
+		Logger->LogInfo(m_name + " Infos: ");
+		Logger->LogInfo("- Parent: " + (GetParent() ? GetParent()->GetName() : "None"));
+		Logger->LogInfo(std::format("- UUID: {}", uuids::to_string(m_uuid)));
 
-        GetTransform().GetData(false);
+		Logger->LogInfo("- Name: " + m_name);
 
-        std::string tags;
-        tags += "- Tags: {";
-        if (m_tags.empty())
-        {
-            tags += " None";
-        }
-        else
-        {
-            for (auto it = m_tags.begin(); it != m_tags.end(); ++it)
-            {
-                tags += " " + *it;
+		GetTransform().GetData(false);
 
-                if (std::next(it) != m_tags.end())
-                {
-                    tags += ",";
-                }
-            }
-        }
-        tags += " }";
-        Logger->LogInfo(tags);
+		std::string tags;
+		tags += "- Tags: {";
+		if (m_tags.empty())
+		{
+			tags += " None";
+		}
+		else
+		{
+			for (auto it = m_tags.begin(); it != m_tags.end(); ++it)
+			{
+				tags += " " + *it;
 
-        std::string status = m_isActive ? "True" : "False";
-        Logger->LogInfo("- Active: " + status);
+				if (std::next(it) != m_tags.end())
+				{
+					tags += ",";
+				}
+			}
+		}
+		tags += " }";
+		Logger->LogInfo(tags);
 
-        if (_ForceLoggerDraw)
-            Logger->Show();
-    }
+		std::string status = m_isActive ? "True" : "False";
+		Logger->LogInfo("- Active: " + status);
 
-    std::vector<IComponent*> GameObject::GetAllComponents() const
-    {
-        return m_components;
-    }
+		if (_ForceLoggerDraw)
+			Logger->Show();
+	}
+
+	std::vector<IComponent*> GameObject::GetAllComponents() const
+	{
+		return m_components;
+	}
 }
