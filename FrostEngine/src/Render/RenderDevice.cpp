f68cd1e387a77c8c5d66aa533745794965455bcf
HideNknow <outin.matthieu@gmail.com>
2024-03-24 22:29:23 +0100

    Update : RenderDevice Can now zoom in and out

--- FrostEngine/src/Render/RenderDevice.cpp
+++ FrostEngine/src/Render/RenderDevice.cpp
@@ -3,6 +3,9 @@
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 
+#include "Core/SignalSlot/Signal.h"
+#include "Core/SignalSlot/Slot.h"
+
 namespace frost::core
 {
 	RenderDevice* RenderDevice::m_instance = nullptr;
@@ -29,20 +32,25 @@ namespace frost::core
 	{
 		m_internal->window = reinterpret_cast<GLFWwindow*>(_window->GetInternal());
 		glewInit();
-
-		
-		m_shaderProgram.InitShader("default.vert", "default.frag");// set shaderProgram of used shader
+	
+		// set shaderProgram of used shader
+		m_shaderProgram.InitShader("default.vert", "default.frag");
+		m_shaderProgram.Enable();
 
 		//for alpha blending
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 		//PickUp Position of the shader in memory
-		m_positionLocation = glGetUniformLocation(m_shaderProgram.m_gl_ID, "uPosition");
-		m_rotationLocation = glGetUniformLocation(m_shaderProgram.m_gl_ID, "uRotation");
-		m_scaleLocation = glGetUniformLocation(m_shaderProgram.m_gl_ID, "uScale");
+		m_positionLocation	  =	glGetUniformLocation(m_shaderProgram.m_gl_ID, "uPosition");
+		m_rotationLocation	  =	glGetUniformLocation(m_shaderProgram.m_gl_ID, "uRotation");
+		m_scaleLocation		  =	glGetUniformLocation(m_shaderProgram.m_gl_ID, "uScale");
 		m_aspectRatioLocation = glGetUniformLocation(m_shaderProgram.m_gl_ID, "uAspectRatio");
-		m_texture = glGetUniformLocation(m_shaderProgram.m_gl_ID, "uTexture");
+		m_texture			  = glGetUniformLocation(m_shaderProgram.m_gl_ID, "uTexture");
+		m_zoom 				  = glGetUniformLocation(m_shaderProgram.m_gl_ID, "uZoom");
+
+		SetZoom(0.1f);
+		SetClearColor(glm::vec4(0.0f,0.0f, 0.0f, 1.0f));
 
 		return m_instance;
 	}
@@ -60,13 +68,12 @@ namespace frost::core
 	void RenderDevice::Update()
 	{
 
-		glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+		glClearColor(m_clearColor.x, m_clearColor.y, m_clearColor.z, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
-		m_shaderProgram.Enable();
 
 		int w, h;
 		glfwGetWindowSize(m_internal->window, &w, &h);
+		glViewport(0, 0, w, h);
 		glProgramUniform1f(m_shaderProgram.m_gl_ID, m_aspectRatioLocation, float(w) / float(h));
 
 		for (int i = 0; i < GetVaoToRender().size(); i++)
@@ -82,14 +89,33 @@ namespace frost::core
 		ClearVaosToRender();
 
 	}
+	void RenderDevice::SetZoom(float _zoom)
+	{
+		glProgramUniform1f(m_shaderProgram.m_gl_ID, m_zoom, _zoom);
+	}
+
+	float RenderDevice::GetZoom()
+	{
+		float toReturn;
+		glGetUniformfv(m_shaderProgram.m_gl_ID, m_zoom, &toReturn);
+		return toReturn;
+	}
+
+	void RenderDevice::SetClearColor(glm::vec4 _color)
+	{
+		m_clearColor = _color;
+	}
+
 	void RenderDevice::AddVAO(VertexArrayObject _newVao)
 	{
 		m_internal->VaoToRender.push_back(_newVao);
 	}
+
 	void RenderDevice::ClearVaosToRender()
 	{
 		m_internal->VaoToRender.clear();
 	}
+
 	std::vector<VertexArrayObject> RenderDevice::GetVaoToRender()
 	{
 		return m_internal->VaoToRender;
--- Game/ressources/shaders/default.vert
+++ Game/ressources/shaders/default.vert
@@ -7,11 +7,12 @@ uniform vec2  uPosition;
 uniform float uRotation;
 uniform vec2  uScale;
 uniform float uAspectRatio;
+uniform float uZoom;
 
 void main()
 {
 	mat2 rotMatrix = mat2(cos(uRotation), -sin(uRotation), sin(uRotation), cos(uRotation));
-	vec2 finalPos = ((rotMatrix * aPos) * uScale + uPosition) / vec2(uAspectRatio, 1.0) * 0.1;
+	vec2 finalPos = ((rotMatrix * aPos) * uScale + uPosition) / vec2(uAspectRatio, 1.0) * uZoom;
 
 	gl_Position = vec4(finalPos, 0.0, 1.0);
 	vColor = aColor;
